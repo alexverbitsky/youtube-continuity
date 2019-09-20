8fff7e1429a2d9ba9f05bc100c85e0f736a3f231
Alexey Verbitsky <leshaverbitsky@gmail.com>
2019-09-20 11:31:58 +0300

    feat: functionality to pick player timestamp implementation

---
+++ content-scripts/content.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener((message, sender, callback) => {
+  
+  const player = document.querySelector('video');
+  const currentTime = Number(player.currentTime);
+  callback(Math.ceil(currentTime));
+
+})
