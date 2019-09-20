4754beab216bd4a9a3a23c17b11785f5dde92acc
Alexey Verbitsky <leshaverbitsky@gmail.com>
2019-09-20 11:32:47 +0300

    feat: qr code generation, video timestamp handling

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,21 @@
-console.log('popup loaded')
+document.querySelector('body').addEventListener('click', event => {
+
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, 'text', {}, function(timestamp) {
+      console.log(tabs)
+
+      const url = tabs[0].url;
+      const videoID = url.match(/watch\?v=(.*)/)[1];
+
+      const resultLink = 'https://youtu.be/' + videoID + '?t=' + timestamp;
+
+      new QRious({
+        element: document.getElementById('qrPlaceholder'),
+        value: resultLink,
+      })
+
+    });
+  });
+
+
+})
